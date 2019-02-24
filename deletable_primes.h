ab1d9f97156dc1098172822db5d57f3e3c3b706c
UltraSaiyan <adam.altalbani@hotmail.com>
2019-02-24 13:12:32 +0100

    Finished

--- deletable_primes.h
+++ deletable_primes.h
@@ -13,4 +13,9 @@
 #ifndef ___DELETABLE_PRIMES
 #define ___DELETABLE_PRIMES
 
+unsigned long remove_digit(int index, unsigned long n);
+int get_ways(unsigned long n);
+bool is_prime(unsigned long n);
+int get_lenght(unsigned long n);
+
 #endif
